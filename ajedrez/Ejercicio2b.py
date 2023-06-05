a134f46db1ab70286d4393d5e29a57e298c0aaf9
carrascoArles <acarrascoch@unsa.edu.pe>
2023-06-04 22:49:06 -0500

    ejercicio 2b hecho

--- ajedrez/Ejercicio2b.py
+++ ajedrez/Ejercicio2b.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+knightN = knight.negative()
+conjunto1 = knight.join(knightN)
+knightN2 = knightN.verticalMirror()
+conjunto2 = knightN2.join(knight.verticalMirror())
+conjunto = conjunto1.up(conjunto2)
+
+draw(conjunto)
