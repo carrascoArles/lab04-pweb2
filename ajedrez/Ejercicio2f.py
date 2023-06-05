f8ac42348052928507f91ff3744f2023b800ca9f
carrascoArles <acarrascoch@unsa.edu.pe>
2023-06-04 23:45:29 -0500

    funcionando eje 2f

--- ajedrez/Ejercicio2f.py
+++ ajedrez/Ejercicio2f.py
@@ -3,7 +3,8 @@ from chessPictures import *
 
 conjunto = square.up(square.negative())
 conjunto2 = conjunto.verticalRepeat(2)
-
-draw(conjunto2)
+conjunto3 = conjunto2.join(conjunto2.negative())
+conjunto4 = conjunto3.horizontalRepeat(4)
+draw(conjunto4)
 
 
