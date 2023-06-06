a8c6f1f2bb2f19e2ad80aaf1c67c8b97b98d14f1
carrascoArles <acarrascoch@unsa.edu.pe>
2023-06-05 21:28:49 -0500

    agregando fichas negras a cuadrados

--- ajedrez/Ejercicio2g.py
+++ ajedrez/Ejercicio2g.py
@@ -23,3 +23,15 @@ bishopSquareN = squareN.under(bishop)
 pawnSquareN = squareN.under(pawn)
 kingSquare = squareN.under(king)
 
+#fichas negras en sus cuadrados
+rockNsquare = square.under(rockN)
+knightNsquare = square.under(knightN)
+bishopNsquare = square.under(bishopN)
+pawnNsquare = square.under(pawnN)
+kingNSquare = square.under(kingN)
+
+rockNsquareN = squareN.under(rockN)
+knightNsquareN = squareN.under(knightN)
+bishopNsquareN = squareN.under(bishopN)
+pawnNsquareN = squareN.under(pawnN)
+queenNSquare = squareN.under(queenN)
