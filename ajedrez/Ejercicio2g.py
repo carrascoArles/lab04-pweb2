b34da571459703fb80e31c026cea4d4d99263735
carrascoArles <acarrascoch@unsa.edu.pe>
2023-06-05 21:25:36 -0500

    agregando fichas blancas a cuadrados

--- ajedrez/Ejercicio2g.py
+++ ajedrez/Ejercicio2g.py
@@ -10,3 +10,16 @@ queenN = queen.negative()
 pawnN = pawn.negative()
 squareN = square.negative()
 
+#fichas blancas en sus cuadrados
+rockSquare = square.under(rock)
+knightSquare = square.under(knight)
+bishopSquare = square.under(bishop)
+pawnSquare = square.under(pawn)
+queenSquare = square.under(queen)
+
+rockSquareN = squareN.under(rock)
+knightSquareN = squareN.under(knight)
+bishopSquareN = squareN.under(bishop)
+pawnSquareN = squareN.under(pawn)
+kingSquare = squareN.under(king)
+
