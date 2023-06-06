79688cef572746c41e1c7eb7e0111dd384183dc9
carrascoArles <acarrascoch@unsa.edu.pe>
2023-06-05 21:24:13 -0500

    agregando variables a usar

--- ajedrez/Ejercicio2g.py
+++ ajedrez/Ejercicio2g.py
@@ -1,2 +1,12 @@
 from interpreter import draw
 from chessPictures import *
+#primero crear todas las variables a usar
+
+bishopN = bishop.negative()
+knightN = knight.negative()
+rockN = rock.negative()
+kingN = king.negative()
+queenN = queen.negative()
+pawnN = pawn.negative()
+squareN = square.negative()
+
